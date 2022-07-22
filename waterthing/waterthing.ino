59bfd5fa82c131e05d9fb3b781ebc40ea6f54873
HACKER3000 <hacker3000@posteo.org>
2022-07-22 14:01:31 +0200

    move gfx to external file for easy sharing between sketches

--- waterthing/waterthing.ino
+++ waterthing/waterthing.ino
@@ -11,6 +11,7 @@ Zyklus:
 
 #include <EEPROM.h>
 #include <LiquidCrystal_I2C.h>
+#include "gfx.h"
 #include <TimeLib.h>
 #include <Wire.h>
 #include <DS1307RTC.h>
@@ -58,42 +59,7 @@ Zyklus:
 LiquidCrystal_I2C lcd(0x27,16,2);
 PCF8574 pcf(LED_PCF_ADDR); //all addr pins to high
 
-//dynamic status
-byte gfx_off[8] = {B01110, B01010, B01110, B00000, B11011, B10010, B11011, B10010};
-byte gfx_idle[8] = {B01110, B10001, B10111, B10101, B01110, B00000, B10101, B00000}; //clock 3 dots version
-//byte gfx_idle[8] = {B00000, B00000, B00000, B00000, B00000, B00000, B10101, B00000}; //... version
-//byte gfx_idle[8] = {B00110, B00011, B01100, B00110, B11000, B01000, B10000, B11000}; //zZZ version
-byte gfx_fill[8] = {B00100, B10101, B01110, B00100, B00000, B10001, B10001, B11111};
-byte gfx_drain[8] = {B10001, B10001, B11111, B00000, B00100, B10101, B01110, B00100};
-byte gfx_afterdrain[8] = {B00000, B10001, B10001, B11111, B00000, B00000, B10101, B00000};
-byte gfx_no_water[8] = {B00001, B00001, B01000, B01101, B01100, B10110, B11110, B01100};
-byte gfx_low_bat[8] = {B01110, B10001, B10101, B10101, B10001, B10101, B10001, B11111};
-byte gfx_rain_detected[8] = {B00100, B00100, B01110, B10111, B11111, B01110, B00000, B10101};
-byte gfx_error[8] = {B10101, B10101, B10101, B10101, B00000, B00000, B10101, B00000};
-
-//symbol graphics
-byte gfx_hyst[8] = {B00111, B01010, B01010, B01010, B01010, B01010, B01010, B11100};
-byte gfx_drop[8] = {B00100, B00100, B01110, B01110, B10111, B11111, B01110, B00000};
-byte gfx_clock[8] = {B01110, B00100, B01110, B10001, B10111, B10101, B01110, B00000};
-byte gfx_flow[8] = {B00100, B01100, B10110, B11110, B01100, B00010, B11001, B00010};
-byte gfx_pulse[8] = {B00000, B01110, B01010, B01010, B01010, B01010, B11011, B00000};
-byte gfx_h3[8] = {B00000, B00000, B10110, B10001, B11110, B10001, B10110, B00000};
-byte gfx_tank_top[8] = {B10011, B01011, B10011, B00011, B00011, B00011, B00011, B00011};
-byte gfx_tank_bottom[8] = {B00011, B00011, B00011, B00011, B00011, B10011, B01011, B10011};
-byte gfx_key[8] = {B01110, B10001, B01110, B00100, B01100, B00100, B01100, B00100};
-byte gfx_radio[8] = {B00010, B01001, B00101, B10101, B00101, B10001, B10000, B10000};
-
-//umlaut graphics
-byte gfx_uml_s[8] = {B01100, B10010, B10010, B11100, B10010, B10010, B10100, B10000};
-byte gfx_uml_a[8] = {B01010, B00000, B01110, B00001, B01111, B10001, B01111, B00000};
-byte gfx_uml_o[8] = {B01010, B00000, B01110, B10001, B10001, B10001, B01110, B00000};
-byte gfx_uml_u[8] = {B01010, B00000, B10001, B10001, B10001, B10011, B01101, B00000};
-
 enum gfx_IDs { //enum for naming display gfx ids
-  /*GFX_ID_IDLE = 0,
-  GFX_ID_NO_WATER = 1, //GFX_ID_DROP = 1,
-  GFX_ID_FILL = 2,
-  GFX_ID_DRAIN = 3,*/
   GFX_ID_STATUS = 0, //redefined dynamically
   GFX_ID_DYN_1 = 1,
   GFX_ID_DYN_2 = 2,
